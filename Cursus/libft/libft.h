3d9242f6644acefa6cf0c4021232238a48a3d53c
Joel <j.roman23@hotmail.com>
2024-09-22 12:18:35 +0200

    Añadidas declaraciones en libft.h

--- Cursus/libft/libft.h
+++ Cursus/libft/libft.h
@@ -15,8 +15,6 @@
 # define LIBFT_H
 
 # include <stdlib.h>
-# include <unistd.h>
-# include <string.h>
 
 void		*ft_memset(void *s, int c, size_t n);
 void 		*ft_memcpy(void *dest, const void *src, size_t n);
@@ -31,12 +29,14 @@ int			ft_isprint(int c);
 int			ft_toupper(int c);
 int			ft_tolower(int c);
 int 		ft_memcmp(const void *s1, const void *s2, size_t n);
+int         ft_strncmp(char *s1, char *s2, size_t n);
 
 char		*ft_strchr(const char *s, int c);
+char    	*ft_strrchr(const char *s, int c);
 char		*ft_strnstr(const char *s1, const char *s2, size_t n);
 
-size_t		strlen(const char *s);
-size_t		strlcpy(char *dst, const char *src, size_t size);
-size_t		strlcat(char *dst, const char *src, size_t size);
+size_t		ft_strlen(const char *s);
+size_t		ft_strlcpy(char *dst, const char *src, size_t size);
+size_t		ft_strlcat(char *dst, const char *src, size_t size);
 
 #endif  // LIBFT_H
